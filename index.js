fa64763742dd0fdf8fb45b54c691053ef6877519
Milind Mishra <milind@Milinds-MacBook-Air.local>
2024-09-04 23:45:45 +0530

    📦 NEW: updated script

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ require("dotenv").config();
 
 const redis = require("redis");
 function main() {
-  console.log("main fn starts here ========================");
+  console.log(process.env.HOST);
+  console.log(process.env.PORT);
   // Replace the following values with your ElastiCache configuration details
   const host = process.env.HOST;
   const port = process.env.PORT; // Default Redis port
 
   // Create the Redis client
-  const redisClient = redis.createClient({ host, port });
-  console.log("after redis client ========================");
+  const redisClient = redis.createClient({
+    url: `redis://${host}:${port}`,
+  });
 
   redisClient.on("connect", () => {
     console.log("Connected to Redis12345");
@@ -18,35 +20,29 @@ function main() {
   redisClient.on("error", (err) => {
     console.log(err.message);
   });
-  console.log("after redis error ========================");
 
   redisClient.on("ready", () => {
     console.log("Redis is ready");
   });
-  console.log("after redis ready ========================");
 
   redisClient.on("end", () => {
     console.log("Redis connection ended");
   });
-  console.log("after redis end ========================");
 
   process.on("SIGINT", () => {
     redisClient.quit();
   });
-  console.log("after redis sigint ========================");
 
   redisClient
     .connect()
     .then(async () => {
       console.log("Connected to Redis");
-      // await redisClient.set('key', 'value23');
-      const value = await redisClient.get("key");
-      console.log(value);
-      console.log("after redis get ========================");
+      await redisClient.set("key2", "milind");
+      const value = await redisClient.get("key2");
+      console.log({ value });
     })
     .catch((err) => {
-      console.log("after redis error catch ========================");
-      console.log("ERROR : ", err.message);
+      console.log(err.message);
     });
 }
 main();
